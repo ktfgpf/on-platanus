2d333cdd1af7b8f40e0af45ebf081c3c956efb14
tkhr <tkhr@tkhrnoMacBook-Pro.local>
2019-08-17 22:12:22 +0900

    Name change

--- platanus-top/wsgi.py
+++ platanus-top/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for platanus project.
+WSGI config for platanus-top project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "platanus.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "platanus-top.settings")
 
 from django.core.wsgi import get_wsgi_application
 
